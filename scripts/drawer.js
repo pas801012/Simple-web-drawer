c4fbf8f114200dce0127885cc02b5ef8b433a134
pas801012 <48752952+pas801012@users.noreply.github.com>
2019-09-30 18:25:30 +0300

    Simple drawing works

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -7,16 +7,43 @@ var context = canvas.getContext('2d');
 
 canvas.style.border = '2px dashed red';
 
+var clickX = [];
+var clickY = [];
+var clickDrag = [];
 var paint;
 
-var addClick = function() {};
-var redraw = function() {};
+var addClick = function(x, y, dragging) {
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+};
+
+var redraw = function() {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+  context.strokeStyle = '#df4b26';
+  context.lineJoin = 'round';
+  context.lineWidth = 5;
+
+  for (var i = 0; i < clickX.length; i++) {
+    context.beginPath();
+    if (clickDrag[i] && i) {
+      context.moveTo(clickX[i - 1], clickY[i - 1]);
+    } else {
+      context.moveTo(clickX[i] - 1, clickY[i]);
+    }
+    context.lineTo(clickX[i], clickY[i]);
+    context.closePath();
+    context.stroke();
+  }
+};
 
 var onMouseDown = function(e) {
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
 
   paint = true;
+  console.log('ranges', mouseX, mouseY);
   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
   redraw();
 };
@@ -32,7 +59,10 @@ var onMouseUp = function(e) {
   paint = false;
 };
 
-var onMouseLeave = function(e) {};
+var onMouseLeave = function(e) {
+  onMouseUp.apply(this, arguments);
+  // paint = false;
+};
 
 canvas.addEventListener('mousedown', onMouseDown);
 canvas.addEventListener('mousemove', onMouseMove);
